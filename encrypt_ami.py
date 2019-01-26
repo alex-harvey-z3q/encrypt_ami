ccd547585710a175478bc5e3599e13268435a1c4
Alex Harvey <alexharv074@gmail.com>
2019-01-26 18:09:08 +1100

    Refactor as a class

--- encrypt_ami.py
+++ encrypt_ami.py
@@ -10,264 +10,261 @@ import json
 import urllib2
 
 def get_args():
+  if 'AWS_DEFAULT_REGION' not in os.environ:
+    raise "Expected to find default region in $AWS_DEFAULT_REGION"
+
   parser = argparse.ArgumentParser()
-  parser.add_argument(
-    "--source-region",
-        action="store", required=False, metavar="SOURCE_AMI_REGION", dest="source_region", default="ap-southeast-2",
-        help="The name of the region that contains the AMI to copy.")
-  parser.add_argument(
-    "--description",
-        action="store", required=False, metavar="DESCRIPTION", dest="description", default="",
-        help="A description for the new AMI in the destination region.")
-  parser.add_argument(
-    "--encrypted",
-        required=False, action="store_true",
-        help="Specifies whether the destination snapshots of the copied image should be encrypted.")
-  parser.add_argument(
-    "--kms-key-id",
-        action="store", required=False, metavar="KMS_KEY_ID", dest="kms_key_id", default="",
-        help="The full ARN of the AWS Key Management Service (AWS KMS) CMK to use when encrypting the snapshots of an image during a copy operation. This parameter is only required if you want to use a non-default CMK; if this parameter is not specified, the default CMK for EBS is used.")
   parser.add_argument(
     "--source-image-id",
         action="store", required=True, metavar="SOURCE_AMI_ID", dest="source_image_id",
-        help="The ID of the AMI to copy.")
+        help="The ID of the AMI to copy")
   parser.add_argument(
     "--name",
-        action="store",
-        help="The name of the new AMI in the destination region.")
+        action="store", required=True, metavar="NAME", dest="image_name",
+        help="The name of the new AMI in the destination region")
   parser.add_argument(
-    "--os",
-        action="store", required=False, metavar="OS", dest="os", default="linux", choices=["linux", "windows"],
-        help="The OS of the source image.")
+    "--kms-key-id",
+        action="store", required=True, metavar="KMS_KEY_ID", dest="kms_key_id",
+        help="The full ARN of the KMS CMK to use when encrypting the snapshots of an image during a copy operation")
+  parser.add_argument(
+    "--iam-instance-profile",
+        action="store", required=False, metavar="IAM_INSTANCE_PROFILE", dest="iam_instance_profile", default="",
+        help="The IAM Instance Profile name e.g. MyInstanceProfile")
+  parser.add_argument(
+    "--subnet-id",
+        action="store", required=False, metavar="SUBNET_ID", dest="subnet_id", default="",
+        help="The subnet ID to launch the image in")
+  parser.add_argument(
+    "--os-type",
+        action="store", required=False, metavar="OS_TYPE", dest="os_type", default="linux", choices=["linux", "windows"],
+        help="The OS of the source image")
   return parser.parse_args()
 
-def build_user_data(os_type):
-  if os_type == 'windows':
-    return ""
-  else:
-    return """
-  <powershell>
-    $Cmd = Get-WmiObject -Class Win32_OperatingSystem | ForEach-Object -MemberName Caption
+class AMIEncrypter():
 
-    $Get_OS = $Cmd -match '(\d+)'
-    if ($Get_OS) {
-      $Os_Type = $matches[1]
-    }
+  def __init__(self):
 
-    Write-Host "The operating system is $Os_Type"
+    if os.environ.get('BOTO_RECORD'):
+      import placebo
+      boto3.setup_default_session()
+      session = boto3.DEFAULT_SESSION
+      pill = placebo.attach(session, data_path='.')
+      pill.record()
 
-    if ($Os_Type -eq '2012') {
-      $EC2SettingsFile = "C:\\Program Files\\Amazon\\Ec2ConfigService\\Settings\\Config.xml"
-      $xml = [xml](get-content $EC2SettingsFile)
-      $xmlElement = $xml.get_DocumentElement()
-      $xmlElementToModify = $xmlElement.Plugins
-      $enableElements = "Ec2SetPassword", `
-        "Ec2SetComputerName", `
-        "Ec2HandleUserData", `
-        "Ec2DynamicBootVolumeSize"
-      $xmlElementToModify.Plugin | Where-Object {$enableElements -contains $_.name} | Foreach-Object {$_.State="Enabled"}
-      $xml.Save($EC2SettingsFile)
+    self.client = boto3.client('ec2')
 
-      # Sysprep Configuration setting for win2k12
-      $EC2SettingsFile = "C:\\Program Files\\Amazon\\Ec2ConfigService\\Settings\\BundleConfig.xml"
-      $xml = [xml](get-content $EC2SettingsFile)
-      $xmlElement = $xml.get_DocumentElement()
-      foreach ($element in $xmlElement.Property) {
-        if ($element.Name -eq "AutoSysprep") {
-          $element.Value = "Yes"
-        }
-      }
-      $xml.Save($EC2SettingsFile)
+  def encrypt(self,
+      source_image_id, image_name, kms_key_id, iam_instance_profile,
+      subnet_id, os_type):
 
-    } elseif ($Os_Type -eq '2016') {
+    try:
+      if self.this_account() == self.account_of(source_image_id):
+        encrypted_image_id = self.copy_image(source_image_id, image_name, kms_key_id)
 
-      # Changes are made to LaunchConfig.json file
-      $LaunchConfigFile = "C:\\ProgramData\\Amazon\\EC2-Windows\\Launch\\Config\\LaunchConfig.json"
-      $jsoncontent = Get-Content $LaunchConfigFile | ConvertFrom-Json
-      $jsoncontent.SetComputerName = 'true'
-      $jsoncontent | ConvertTo-Json  | set-content $LaunchConfigFile
+      else:
+        instance_id = self.run_instance(source_image_id, iam_instance_profile, subnet_id, os_type)
+        self.stop_instance(instance_id)
+        unencrypted_image_id = self.create_image(instance_id, image_name + "-unencrypted")
+        self.terminate_instance(instance_id)
+        encrypted_image_id = self.copy_image(source_image_id, image_name, kms_key_id)
+        self.deregister_image(unencrypted_image_id)
 
-      # This script schedules the instance to initialize during the next boot.
-      C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\InitializeInstance.ps1 -Schedule
+    except KeyboardInterrupt:
+      sys.exit("User aborted script!")
 
-      # The EC2Launch service runs Sysprep, a Microsoft tool that enables creation of customized Windows AMI that can be reused
-      C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\SysprepInstance.ps1
-
-    } else {
-      Write-Host "Don't know what to do for OS type $Os_Type"
-    }
-  </powershell>
-  """
-
-def this_account():
-  return boto3.client('sts').get_caller_identity().get('Account')
-
-def account_of(image_id):
-  client = boto3.client('ec2')
-  response = client.describe_images(DryRun=False, ImageIds=[image_id])
-  return response['Images'][0]['ImageLocation'].split('/')[0]
-
-def copy_image(image_id, name, kms_key_id):
-  client = boto3.client('ec2')
-
-  print "Creating the AMI: %s" % name
-
-  response = client.copy_image(
-          Name=name,
-          SourceImageId=image_id,
-          DryRun=False,
-          SourceRegion='ap-southeast-2',
-          Encrypted=True,
-          KmsKeyId=kms_key_id)
-
-  encrypted_image_id = response['ImageId']
-  wait_for_image_state(encrypted_image_id, 'available')
-
-  return encrypted_image_id
-
-def create_image(instance_id, name):
-  client = boto3.client('ec2')
-
-  print "Creating the AMI: %s" % name
-
-  response = client.create_image(
-          InstanceId=instance_id,
-          Name=name)
-
-  unencrypted_image_id = response['ImageId']
-  wait_for_image_state(unencrypted_image_id, 'available')
-
-  return unencrypted_image_id
-
-def deregister_image(image_id):
-  client = boto3.client('ec2')
-  print "Deregistering the AMI: %s" % image_id
-  return client.deregister_image(DryRun=False, ImageId=image_id)
-
-def wait_for_image_state(image_id, desired_state, **kwargs):
-  client = boto3.client('ec2')
-
-  print "Waiting for AMI to become %s..." % desired_state
-
-  while True:
-    response = client.describe_images(DryRun=False, ImageIds=[image_id])
-    state = response['Images'][0]['State']
-
-    if state == desired_state:
-      break
+  def build_user_data(self, os_type):
+    if os_type == 'windows':
+      return ""
     else:
+      return """
+    <powershell>
+      $Cmd = Get-WmiObject -Class Win32_OperatingSystem | ForEach-Object -MemberName Caption
+
+      $Get_OS = $Cmd -match '(\d+)'
+      if ($Get_OS) {
+        $Os_Type = $matches[1]
+      }
+
+      Write-Host "The operating system is $Os_Type"
+
+      if ($Os_Type -eq '2012') {
+        $EC2SettingsFile = "C:\\Program Files\\Amazon\\Ec2ConfigService\\Settings\\Config.xml"
+        $xml = [xml](get-content $EC2SettingsFile)
+        $xmlElement = $xml.get_DocumentElement()
+        $xmlElementToModify = $xmlElement.Plugins
+        $enableElements = "Ec2SetPassword", `
+          "Ec2SetComputerName", `
+          "Ec2HandleUserData", `
+          "Ec2DynamicBootVolumeSize"
+        $xmlElementToModify.Plugin | Where-Object {$enableElements -contains $_.name} | Foreach-Object {$_.State="Enabled"}
+        $xml.Save($EC2SettingsFile)
+
+        # Sysprep Configuration setting for win2k12
+        $EC2SettingsFile = "C:\\Program Files\\Amazon\\Ec2ConfigService\\Settings\\BundleConfig.xml"
+        $xml = [xml](get-content $EC2SettingsFile)
+        $xmlElement = $xml.get_DocumentElement()
+        foreach ($element in $xmlElement.Property) {
+          if ($element.Name -eq "AutoSysprep") {
+            $element.Value = "Yes"
+          }
+        }
+        $xml.Save($EC2SettingsFile)
+
+      } elseif ($Os_Type -eq '2016') {
+
+        # Changes are made to LaunchConfig.json file
+        $LaunchConfigFile = "C:\\ProgramData\\Amazon\\EC2-Windows\\Launch\\Config\\LaunchConfig.json"
+        $jsoncontent = Get-Content $LaunchConfigFile | ConvertFrom-Json
+        $jsoncontent.SetComputerName = 'true'
+        $jsoncontent | ConvertTo-Json  | set-content $LaunchConfigFile
+
+        # This script schedules the instance to initialize during the next boot.
+        C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\InitializeInstance.ps1 -Schedule
+
+        # The EC2Launch service runs Sysprep, a Microsoft tool that enables creation of customized Windows AMI that can be reused
+        C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\SysprepInstance.ps1
+
+      } else {
+        Write-Host "Don't know what to do for OS type $Os_Type"
+      }
+    </powershell>
+    """
+
+  def this_account(self):
+    return boto3.client('sts').get_caller_identity().get('Account')
+
+  def account_of(self, image_id):
+    response = self.client.describe_images(DryRun=False, ImageIds=[image_id])
+    return response['Images'][0]['ImageLocation'].split('/')[0]
+
+  def copy_image(self, image_id, name, kms_key_id):
+    print "Creating the AMI: %s" % name
+
+    response = self.client.copy_image(
+            Name=name,
+            SourceImageId=image_id,
+            DryRun=False,
+            SourceRegion=os.environ.get('AWS_DEFAULT_REGION'),
+            Encrypted=True,
+            KmsKeyId=kms_key_id)
+
+    encrypted_image_id = response['ImageId']
+    self.wait_for_image_state(encrypted_image_id, 'available')
+
+    return encrypted_image_id
+
+  def create_image(self, instance_id, name):
+    print "Creating the AMI: %s" % name
+
+    response = self.client.create_image(
+            InstanceId=instance_id,
+            Name=name)
+
+    unencrypted_image_id = response['ImageId']
+    self.wait_for_image_state(unencrypted_image_id, 'available')
+
+    return unencrypted_image_id
+
+  def deregister_image(self, image_id):
+    print "Deregistering the AMI: %s" % image_id
+    return self.client.deregister_image(DryRun=False, ImageId=image_id)
+
+  def wait_for_image_state(self, image_id, desired_state, **kwargs):
+    print "Waiting for AMI to become %s..." % desired_state
+
+    while True:
+      response = self.client.describe_images(DryRun=False, ImageIds=[image_id])
+      state = response['Images'][0]['State']
+
+      if state == desired_state:
+        break
+      else:
+        print "state: %s" % state
+
+      time.sleep(10)
+
+  def wait_for_instance_status(self, instance_id, desired_state, desired_status=''):
+    print "Waiting for instance (%s) to become %s..." % (instance_id, desired_state)
+
+    while True:
+      response = self.client.describe_instances(DryRun=False, InstanceIds=[instance_id])
+      state = response['Reservations'][0]['Instances'][0]['State']['Name']
+      if state == desired_state:
+        break
       print "state: %s" % state
+      time.sleep(5)
 
-    time.sleep(10)
+    if not desired_status:
+      return
 
-def wait_for_instance_status(instance_id, desired_state, desired_status=''):
-  client = boto3.client('ec2')
+    print "Waiting for instance (%s) to become %s..." % (instance_id, desired_status)
 
-  print "Waiting for instance (%s) to become %s..." % (instance_id, desired_state)
+    while True:
+      response = self.client.describe_instance_status(DryRun=False, InstanceIds=[instance_id])
+      status = response['InstanceStatuses'][0]['SystemStatus']['Status']
+      if status == desired_status:
+        break
+      print "state: %s" % status
+      time.sleep(5)
 
-  while True:
-    response = client.describe_instances(DryRun=False, InstanceIds=[instance_id])
-    state = response['Reservations'][0]['Instances'][0]['State']['Name']
-    if state == desired_state:
-      break
-    print "state: %s" % state
-    time.sleep(5)
-
-  if not desired_status:
     return
 
-  print "Waiting for instance (%s) to become %s..." % (instance_id, desired_status)
+  def run_instance(self, image_id, iam_instance_profile, subnet_id, os_type):
+    user_data = build_user_data(os_type)
 
-  while True:
-    response = client.describe_instance_status(DryRun=False, InstanceIds=[instance_id])
-    status = response['InstanceStatuses'][0]['SystemStatus']['Status']
-    if status == desired_status:
-      break
-    print "state: %s" % status
-    time.sleep(5)
+    print "Launching a source AWS instance..."
 
-  return
+    # FIXME. Tags missing. It looks something like:
+    #
+    #   TagSpecifications=[
+    #     {
+    #       'ResourceType': 'instance'
+    #       'Tags': [
+    #         {
+    #           'Key': 'string',
+    #           'Value': 'string'
+    #         },
+    #       ]
+    #     },
+    #   ]
+    #
 
-def run_instance(image_id, iam_instance_profile, subnet_id, os_type):
-  client = boto3.client('ec2')
+    response = self.client.run_instances(ImageId=image_id,
+          InstanceType='c4.2xlarge',
+          IamInstanceProfile={'Name': iam_instance_profile},
+          UserData=user_data,
+          SubnetId=subnet_id,
+          MinCount=1,
+          MaxCount=1)
 
-  user_data = build_user_data(os_type)
+    instance_id = response['Instances'][0]['InstanceId']
+    # create_tags(instance_id, **kwargs)
+    self.wait_for_instance_status(instance_id, 'running', 'ok')
+    return instance_id
 
-  print "Launching a source AWS instance..."
+  # def create_tags(instance_id, **kwargs):
+  #   self.client.create_tags(
+  #           DryRun=False,
+  #           Resources=[instance_id],
+  #           Tags=[{"Key": "Foo", "Value": "Bar"}])
+  #   return
 
-  # FIXME. Tags missing. It looks something like:
-  #
-  #   TagSpecifications=[
-  #     {
-  #       'ResourceType': 'instance'
-  #       'Tags': [
-  #         {
-  #           'Key': 'string',
-  #           'Value': 'string'
-  #         },
-  #       ]
-  #     },
-  #   ]
-  #
+  def stop_instance(self, instance_id):
+    print("Stopping the source AWS instance...")
+    response = self.client.stop_instances(DryRun=False, InstanceIds=[instance_id])
+    self.wait_for_instance_status(instance_id, 'stopped')
 
-  response = client.run_instances(ImageId=image_id,
-        InstanceType='c4.2xlarge',
-        IamInstanceProfile={'Name': iam_instance_profile},
-        UserData=user_data,
-        SubnetId=subnet_id,
-        MinCount=1,
-        MaxCount=1)
+  def terminate_instance(self, instance_id):
+    print("Terminating the source AWS instance...")
+    response = self.client.terminate_instances(DryRun=False, InstanceIds=[instance_id])
+    self.wait_for_instance_status(instance_id, 'terminated')
 
-  instance_id = response['Instances'][0]['InstanceId']
-  # create_tags(instance_id, **kwargs)
-  wait_for_instance_status(instance_id, 'running', 'ok')
-  return instance_id
-
-# def create_tags(instance_id, **kwargs):
-#   client = boto3.client('ec2')
-#   client.create_tags(
-#           DryRun=False,
-#           Resources=[instance_id],
-#           Tags=[{"Key": "Foo", "Value": "Bar"}])
-#   return
-
-def stop_instance(instance_id):
-  client = boto3.client('ec2')
-  print("Stopping the source AWS instance...")
-  response = client.stop_instances(DryRun=False, InstanceIds=[instance_id])
-  wait_for_instance_status(instance_id, 'stopped')
-
-def terminate_instance(instance_id):
-  client = boto3.client('ec2')
-  print("Terminating the source AWS instance...")
-  response = client.terminate_instances(DryRun=False, InstanceIds=[instance_id])
-  wait_for_instance_status(instance_id, 'terminated')
-
-def main():
-  if os.environ.get('BOTO_RECORD'):
-    import placebo
-    boto3.setup_default_session()
-    session = boto3.DEFAULT_SESSION
-    pill = placebo.attach(session, data_path='.')
-    pill.record()
-
-  args = get_args()
-  try:
-    if this_account() == account_of(args.source_image_id):
-      encrypted_image_id = \
-        copy_image(args.source_image_id, args.image_name, args.kms_key_id)
-    else:
-      instance_id = run_instance(args.source_image_id, args.iam_instance_profile,
-        args.subnet_id, args.os_type)
-      stop_instance(instance_id)
-      unencrypted_image_id = \
-        create_image(args.instance_id, args.image_name + "-unencrypted")
-      terminate_instance(instance_id)
-      encrypted_image_id = \
-        copy_image(args.source_image_id, args.image_name, args.kms_key_id)
-      deregister_image(unencrypted_image_id)
-  except KeyboardInterrupt:
-    sys.exit("User aborted script!")
-
-if __name__ == '__main__':
-  main()
+args = get_args()
+ami_encrypter = AMIEncrypter()
+ami_encrypter.encrypt(
+  args.source_image_id,
+  args.image_name,
+  args.kms_key_id,
+  args.iam_instance_profile,
+  args.subnet_id,
+  args.os_type)
